9525ed297fa70765c64b1a53987f851f56bc9b1d
David Kozlik <davidkozlik@outlook.com>
2019-05-25 08:43:53 +0200

    Part1

---
+++ primary/primary/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace primary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I'm trying to be programmer");
+        }
+    }
+}
